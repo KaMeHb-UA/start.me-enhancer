6019854609de71553e5dc9cdacdebc753b32f526
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-25 17:43:35 +0300

    Added auto-executing and removed pseudo-globals

--- loader.js
+++ loader.js
@@ -1,5 +1,4 @@
 (function(){
-    var window = window.parent, document = window.document;
     alert('Hello from ' + window.location);
     if(confirm('Go to GitHub repository?')) window.location = 'https://github.com/KaMeHb-UA/start.me-enhancer';
-})
+})()
