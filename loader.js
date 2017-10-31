88cde511e6a36b4f8ee85f359eea718cc20e8c76
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-31 16:06:21 +0200

    Debug update

--- loader.js
+++ loader.js
@@ -20,4 +20,18 @@
         }
     }
     turnConsole();
+    if(!window['replacing main page initialized']){
+        var body = $('body'),
+            head = $('head');
+
+        window['replacing main page initialized'] = true;
+
+        body.html('<iframe id="replacedPage" src="https://start.me"></iframe>');
+        head.html('');
+        body.css('margin', '0').css('padding', '0').css('border', '0').css('height', '100%');
+        body.children('iframe').css('margin', '0').css('padding', '0').css('border', '0').css('width', '100%').css('height', '100%').load(()=>{
+            $(this).contents().find('html').css('overflow', 'hidden')
+        });
+        $('html').css('height', '100%');
+    }
 })()
